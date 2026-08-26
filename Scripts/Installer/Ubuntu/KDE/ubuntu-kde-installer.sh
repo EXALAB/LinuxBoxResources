7d7ad9057fd539483cb0d0ee6fd166294d8a0458
exalab999 <exalabdevelopers@gmail.com>
2026-08-26 18:03:57 +0800

    Update

--- Scripts/Installer/Ubuntu/KDE/ubuntu-kde-installer.sh
+++ Scripts/Installer/Ubuntu/KDE/ubuntu-kde-installer.sh
@@ -9,7 +9,7 @@ rm -f $tarball
 if [ "$first" != 1 ];then
 	if [ ! -f $tarball ]; then
 		echo "Download Rootfs, this may take a while base on your internet speed."
-		wget --show-progress "https://github.com/NextAppsLab/LinuxBoxResources/releases/download/Ubuntu_KDE/ubuntu-kde-rootfs.tar.xz" -O $tarball
+		wget --show-progress "https://github.com/NextAppsLab/LinuxBoxResources/releases/download/Ubuntu-KDE/ubuntu-kde-rootfs.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
